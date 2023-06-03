e916526073692b8b006fa64c6a02cd2d28941c25
test.user <test.user@lippert.hu>
2023-06-03 09:25:05 +0200

    magasság beállítása a 2 futóra inicializációkor

--- src/main/java/hu/gde/runnersdemo/DataLoader.java
+++ src/main/java/hu/gde/runnersdemo/DataLoader.java
@@ -19,6 +19,7 @@ public class DataLoader implements CommandLineRunner {
         RunnerEntity runnerEntity = new RunnerEntity();
         runnerEntity.setRunnerName("Tomi");
         runnerEntity.setAveragePace(310);
+        runnerEntity.setHeight(185);
 
         LapTimeEntity laptime1 = new LapTimeEntity();
         laptime1.setLapNumber(1);
@@ -38,6 +39,7 @@ public class DataLoader implements CommandLineRunner {
         RunnerEntity runnerEntity2 = new RunnerEntity();
         runnerEntity2.setRunnerName("Zsuzsi");
         runnerEntity2.setAveragePace(290);
+        runnerEntity2.setHeight(190);
 
         LapTimeEntity laptime3 = new LapTimeEntity();
         laptime3.setLapNumber(1);

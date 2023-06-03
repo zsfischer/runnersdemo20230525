c07590719b8129355e472ab68065f3a64081956d
test.user <test.user@lippert.hu>
2023-06-03 08:58:57 +0200

    init

---
+++ src/main/java/hu/gde/runnersdemo/RunnerController.java
@@ -0,0 +1,69 @@
+package hu.gde.runnersdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class RunnerController {
+
+    @Autowired
+    private RunnerRepository runnerRepository;
+    @Autowired
+    private LapTimeRepository lapTimeRepository;
+    @GetMapping("/runners")
+    public String getAllRunners(Model model) {
+        List<RunnerEntity> runners = runnerRepository.findAll();
+        model.addAttribute("runners", runners);
+        return "runners";
+    }
+
+    @GetMapping("/runner/{id}")
+    public String getRunnerById(@PathVariable Long id, Model model) {
+        RunnerEntity runner = runnerRepository.findById(id).orElse(null);
+        RunnerService runnerService = new RunnerService(runnerRepository);
+        if (runner != null) {
+            model.addAttribute("runner", runner);
+            double averageLaptime = runnerService.getAverageLaptime(runner.getRunnerId());
+            model.addAttribute("averageLaptime", averageLaptime);
+            return "runner";
+        } else {
+            return "error";
+        }
+    }
+
+    @GetMapping("/runner/{id}/addlaptime")
+    public String showAddLaptimeForm(@PathVariable Long id, Model model) {
+        RunnerEntity runner = runnerRepository.findById(id).orElse(null);
+        if (runner != null) {
+            model.addAttribute("runner", runner);
+            LapTimeEntity laptime = new LapTimeEntity();
+            laptime.setLapNumber(runner.getLaptimes().size() + 1);
+            model.addAttribute("laptime", laptime);
+            return "addlaptime";
+        } else {
+            return "error";
+        }
+    }
+    @PostMapping("/runner/{id}/addlaptime")
+    public String addLaptime(@PathVariable Long id, @ModelAttribute LapTimeEntity laptime) {
+        RunnerEntity runner = runnerRepository.findById(id).orElse(null);
+        if (runner != null) {
+            laptime.setRunner(runner);
+            laptime.setId(null);
+            runner.getLaptimes().add(laptime);
+            runnerRepository.save(runner);
+        } else {
+            return "error";
+        }
+        return "redirect:/runner/" + id;
+    }
+
+}
+
---
+++ src/main/java/hu/gde/runnersdemo/RunnerEntity.java
@@ -0,0 +1,49 @@
+package hu.gde.runnersdemo;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class RunnerEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long runnerId;
+    private String runnerName;
+    private long averagePace;
+
+    @OneToMany(mappedBy = "runner", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<LapTimeEntity> laptimes = new ArrayList<>();
+
+
+    public RunnerEntity() {
+    }
+
+    public long getRunnerId() {
+        return runnerId;
+    }
+
+    public String getRunnerName() {
+        return runnerName;
+    }
+
+    public long getAveragePace() {
+        return averagePace;
+    }
+
+    public void setRunnerId(long runnerId) {
+        this.runnerId = runnerId;
+    }
+
+    public void setRunnerName(String runnerName) {
+        this.runnerName = runnerName;
+    }
+
+    public void setAveragePace(long averagePace) {
+        this.averagePace = averagePace;
+    }
+
+    public List<LapTimeEntity> getLaptimes() {
+        return laptimes;
+    }
+}
